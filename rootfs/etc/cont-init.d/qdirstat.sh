d3a4245adff04375926b8ca8e318caaaa4919b64
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-08-10 13:30:04 -0400

    Generate machine-id only if not already present.

--- rootfs/etc/cont-init.d/qdirstat.sh
+++ rootfs/etc/cont-init.d/qdirstat.sh
@@ -3,12 +3,18 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
+
 # Make sure mandatory directories exist.
 mkdir -p /config/log
 
 # Generate machine id.
-echo "Generating machine-id..."
-cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
+if [ ! -f /etc/machine-id ]; then
+    log "generating machine-id..."
+    cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
+fi
 
 # Take ownership of the config directory content.
 chown -R $USER_ID:$GROUP_ID /config/*
