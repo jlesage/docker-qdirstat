190973ef8bfddf11ff23847b5b58587eaa3c2c27
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-09-18 13:12:33 -0400

    Make sure to set ownership on all files/folders, event hidden ones.

--- rootfs/etc/cont-init.d/qdirstat.sh
+++ rootfs/etc/cont-init.d/qdirstat.sh
@@ -17,6 +17,6 @@ if [ ! -f /etc/machine-id ]; then
 fi
 
 # Take ownership of the config directory content.
-chown -R $USER_ID:$GROUP_ID /config/*
+find /config -mindepth 1 -exec chown $USER_ID:$GROUP_ID {} \;
 
 # vim: set ft=sh :
