29ed3c5127dbd612b158b76460a900b61b001a2f
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-23 21:39:00 -0400

    Prevent error message in log about missing swrast_dri.so.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 export HOME=/config
+
+# Added to avoid the following error message:
+#   MESA-LOADER: failed to open swrast: Error loading shared library
+#   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
+#   (search paths /usr/lib/xorg/modules/dri, suffix _dri)
+# We could instead install `mesa-dri-gallium`, but this increases the image
+# size a lot.
+export QT_QPA_PLATFORM=xcb
+export QT_XCB_GL_INTEGRATION=none
+
 cd /storage
 exec /usr/bin/qdirstat
 # vim:ft=sh:ts=4:sw=4:et:sts=4
